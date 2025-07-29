f3ff92b5634f2ec20cdb9e2d297b3f73542e8a40
camiloancastro <camiloandrescastro.dev@gmail.com>
2025-07-28 21:14:32 -0500

    Exercise #21 leet-code mezclar dos listas ordenadas

---
+++ src/easy/MergedTwoSortedList21.java
@@ -0,0 +1,49 @@
+package easy;
+
+public class MergedTwoSortedList21 {
+
+  public static void main(String[] args) {
+
+    ListNode node1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+    ListNode node2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+
+    ListNode result = mergeTwoLists(node1, node2);
+
+    imprimirLista(result);
+
+  }
+
+  public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+
+    if (list1 == null) return list2;
+
+    if (list2 == null) return list1;
+
+    if (list1.val < list2.val) {
+      list1.next = mergeTwoLists(list1.next, list2);
+      return list1;
+    } else {
+      list2.next = mergeTwoLists(list1, list2.next);
+      return list2;
+    }
+
+  }
+
+  public static void imprimirLista(ListNode head) {
+    while (head != null) {
+      System.out.print(head.val + " ");
+      head = head.next;
+    }
+    System.out.println();
+  }
+
+
+  public static class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+
+}
