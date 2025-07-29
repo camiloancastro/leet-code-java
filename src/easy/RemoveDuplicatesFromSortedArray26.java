9a39277f34d4ef02b72d19e4a3b0aab9edb31d96
camiloancastro <camiloandrescastro.dev@gmail.com>
2025-07-29 18:40:08 -0500

    Exercise #26 leet-code contar la cantidad de elementos unicos en un array.

---
+++ src/easy/RemoveDuplicatesFromSortedArray26.java
@@ -0,0 +1,30 @@
+package easy;
+
+import java.util.HashSet;
+
+
+public class RemoveDuplicatesFromSortedArray26 {
+
+  public static void main(String[] args) {
+
+    var nums = new int[] {1, 1, 2};
+
+    System.out.println(removeDuplicates(nums));
+
+  }
+
+  public static int removeDuplicates(int[] nums) {
+    if (nums.length == 0) return 0;
+
+    int i = 0;
+    for (int j = 1; j < nums.length; j++) {
+      if (nums[j] != nums[i]) {
+        i++;
+        nums[i] = nums[j];
+      }
+    }
+    return i + 1;
+  }
+
+
+}
